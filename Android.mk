0735a695e87d218d2cafa3609e2a014ae7f38494
Giovix92 <giovix92@giovix92.com>
2024-01-29 16:20:30 +0900

    denniz: Update lk lk2 preloader + Enable warning when included

--- Android.mk
+++ Android.mk
@@ -1,8 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),RMX3031)
+ifeq ($(TARGET_DEVICE),denniz)
 $(foreach f, $(notdir $(wildcard $(LOCAL_PATH)/*)), \
     $(call add-radio-file,$(f)))
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+$(warning denniz firmware has included!!)
+
 endif
